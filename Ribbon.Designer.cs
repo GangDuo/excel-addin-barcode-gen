6ec6eb0675019b28e3fcb42ce3b5bd3ff486b946
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2017-12-29 10:21:45 +0900

    バーコードが入力されたセルにバーコード画像を重ねる。

---
+++ Ribbon.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace BarcodeGen
+{
+    partial class Ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// デザイナー変数が必要です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナーのサポートに必要なメソッドです。
+        /// このメソッドの内容をコード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Office.Tools.Ribbon.RibbonDialogLauncher ribbonDialogLauncherImpl1 = this.Factory.CreateRibbonDialogLauncher();
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabInsert";
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabInsert";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.DialogLauncher = ribbonDialogLauncherImpl1;
+            this.group1.Items.Add(this.button1);
+            this.group1.Label = "ﾊﾞｰｺｰﾄﾞ";
+            this.group1.Name = "group1";
+            this.group1.DialogLauncherClick += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.group1_DialogLauncherClick);
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Label = "画像";
+            this.button1.Name = "button1";
+            this.button1.OfficeImageId = "BarcodeInsert";
+            this.button1.ScreenTip = "JANｺｰﾄﾞからﾊﾞｰｺｰﾄﾞ画像を生成しｼｰﾄに貼り付けます。";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // Ribbon
+            // 
+            this.Name = "Ribbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon Ribbon
+        {
+            get { return this.GetRibbon<Ribbon>(); }
+        }
+    }
+}
